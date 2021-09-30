3487fc5ebd7bb36106e017a526f4e75eabbdf78d
Anastasiya <Tsybusova>
2021-09-30 23:32:00 +0300

    Getting started with homework

--- descryptors.py
+++ descryptors.py
@@ -1,23 +1,30 @@
 import logging
-import sys
-
-# Инициализиция логера
-# метод определения модуля, источника запуска.
-if sys.argv[0].find('client') == -1:
-    # если не клиент то сервер!
-    logger = logging.getLogger('server')
-else:
-    # ну, раз не сервер, то клиент
-    logger = logging.getLogger('client')
+import ipaddress
 
 
-# Дескриптор для описания порта:
-class Port:
+logger = logging.getLogger('server')
+
+class Port():
     def __set__(self, instance, value):
-        if not 1023 < value < 65536:
-            logger.critical(
-                f'Попытка запуска с указанием неподходящего порта {value}. Допустимы адреса с 1024 до 65535.')
+        if value < 65536 or value > 1024:
+            LOG.critical(
+                f'Попытка запуска сервера с указанием неподходящего порта: {value}. Допустимы адреса с 1024 до 65535.')
             exit(1)
+        instance.__dict__[self.name] = value
+
+    def __set_name__(self, owner, name):
+        self.name = name
+
+
+class Addr():
+    def __set__(self, instance, value):
+        if value:
+            try:
+                ip = ipaddress.ip_address(value)
+            except ValueError as e:
+                LOG.critical(
+                    f'Неверно введен IP-адрес: {e}')
+                exit(1)
         instance.__dict__[self.name] = value
 
     def __set_name__(self, owner, name):
